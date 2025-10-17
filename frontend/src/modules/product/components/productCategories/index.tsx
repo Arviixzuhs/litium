2fe27f55d1b3a7c88cf6b35212d9a58e40c585ec
Arviixzuh <arviixzuh@gmail.com>
2025-10-16 21:58:38 -0400

    feat: 🌱 fetch product categories from api and show spinner while loading

--- frontend/src/modules/product/components/productCategories/index.tsx
+++ frontend/src/modules/product/components/productCategories/index.tsx
@@ -1,23 +1,30 @@
+import React from 'react'
 import { Button } from '@heroui/button'
-
-export const categories = [
-  'Todos',
-  'Laptops',
-  'Smartphones',
-  'Audio',
-  'Tablets',
-  'Wearables',
-  'TVs',
-]
+import { Spinner } from '@heroui/react'
+import { CategoryModel } from '@/types/categoryModel'
+import { reqGetCategories } from '@/modules/admin/pages/category/services'
 
 export const ProductCategories = () => {
+  const [categories, setCategories] = React.useState<CategoryModel[] | null>(null)
+  const [isLoading, setIsLoading] = React.useState<boolean>(false)
+
+  React.useEffect(() => {
+    setIsLoading(true)
+    reqGetCategories()
+      .then((res) => setCategories(res.data.content))
+      .catch(console.log)
+      .finally(() => setIsLoading(false))
+  }, [])
+
   return (
     <div className='flex flex-wrap gap-2'>
-      {categories.map((category) => (
-        <Button key={category} className='rounded-full' size='sm' color='primary' variant='flat'>
-          {category}
-        </Button>
-      ))}
+      {isLoading && <Spinner />}
+      {!isLoading &&
+        categories?.map((item, index) => (
+          <Button key={index} className='rounded-full' size='sm' color='primary' variant='flat'>
+            {item.name}
+          </Button>
+        ))}
     </div>
   )
 }
