02d6d336076ba4a6f6b2f10791f3b22a23a148fb
Arviixzuh <arviixzuh@gmail.com>
2025-10-16 22:04:32 -0400

    feat: 🛒 add ShoppingCartModel and related types

---
+++ frontend/src/types/shoppingCartModel.ts
@@ -0,0 +1,37 @@
+import { InvoiceModel } from './invoiceModel'
+import { MessageModel } from './messageModal'
+import { ProductModel } from './productModel'
+import { UserModel } from './userModel'
+
+export enum ShoppingCartStatus {
+  CONFIRMED = 'CONFIRMED',
+  EDITING = 'EDITING',
+}
+
+export interface ShoppingCartProductModel {
+  id: number
+  quantity: number
+  invoiceId?: number | null
+  productId: number
+  shoppingCartId: number
+  createdAt: Date
+
+  product?: ProductModel
+  invoice?: InvoiceModel
+  shoppingCart?: ShoppingCartModel
+}
+
+export interface ShoppingCartModel {
+  id: number
+  name: string
+  userId: number
+  status: ShoppingCartStatus
+  isDeleted?: boolean
+  deletedAt?: Date | null
+  createdAt: string
+
+  // Relaciones
+  user?: UserModel
+  products?: ShoppingCartProductModel[]
+  messages?: MessageModel[]
+}
