189ec4ac65c863834b91360a04b9dccdc17e7c76
Arviixzuh <arviixzuh@gmail.com>
2025-10-16 21:48:45 -0400

    feat: 🛒 add request to get shopping cart by id

--- frontend/src/api/requests.ts
+++ frontend/src/api/requests.ts
@@ -1,4 +1,9 @@
+import { ShoppingCartModel } from '@/types/shoppingCartModel'
 import { api } from './axios'
 
 export const reqAuthLoadProfileByToken = async (token: string) =>
   api.get('/auth/load/profile/' + token)
+
+// Rutas de carrito de compras
+export const reqGetShoppingCartById = (cartId: number) =>
+  api.get<ShoppingCartModel>(`/shopping-cart/${cartId}`)
