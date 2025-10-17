7a1de0ce447f5bae9cb0b3b94afa39f1f601736c
Arviixzuh <arviixzuh@gmail.com>
2025-10-16 21:32:20 -0400

    feat: 🌱 add MessagesController to MessagesModule

--- backend/src/modules/messages/message.module.ts
+++ backend/src/modules/messages/message.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+import { PrismaService } from '@/prisma/prisma.service'
 import { MessagesGateway } from './message.gateway'
 import { MessagesService } from './message.service'
-import { PrismaService } from '@/prisma/prisma.service'
+import { MessagesController } from './message.controller'
 
 @Module({
+  controllers: [MessagesController],
   providers: [MessagesGateway, MessagesService, PrismaService],
 })
 export class MessagesModule {}
