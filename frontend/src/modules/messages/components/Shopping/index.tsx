dce8cb2a65b60abc1a52533a7f392c1b9d1f9cca
Arviixzuh <arviixzuh@gmail.com>
2025-10-16 21:57:20 -0400

    feat: 🌱 fetch shopping cart by id with loading state

--- frontend/src/modules/messages/components/Shopping/index.tsx
+++ frontend/src/modules/messages/components/Shopping/index.tsx
@@ -1,37 +1,49 @@
-import { RootState } from '@/store'
-import { useSelector } from 'react-redux'
-import { Card, CardBody, CardHeader, Image } from '@heroui/react'
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { ShoppingCartModel } from '@/types/shoppingCartModel'
+import { reqGetShoppingCartById } from '@/api/requests'
+import { Card, CardBody, CardHeader, Image, Spinner } from '@heroui/react'
 
 export const Shopping = () => {
-  const { cartItems } = useSelector((state: RootState) => state.shoppingCart)
+  const params = useParams<{ cartId: string }>()
+  const [isLoading, setIsLoading] = React.useState<boolean>(true)
+  const [shoppingCart, setShoppingCart] = React.useState<ShoppingCartModel | null>(null)
+
+  React.useEffect(() => {
+    if (!params.cartId) return
+    reqGetShoppingCartById(Number(params.cartId))
+      .then((res) => setShoppingCart(res.data))
+      .catch(console.log)
+      .finally(() => setIsLoading(false))
+  }, [])
 
   return (
-    <Card
-      classNames={{
-        base: 'w-[500px]',
-      }}
-    >
+    <Card>
       <CardHeader className='font-bold'>Compras</CardHeader>
       <CardBody>
-        {cartItems.map((item, index) => (
-          <li key={index} className='justify-between flex gap-2  items-center'>
-            <div className='flex gap-2  items-center'>
-              <div>
-                <Image
-                  src={'https://heroui.com/images/hero-card-complete.jpeg'}
-                  alt={item.name}
-                  radius='sm'
-                  className='object-cover max-w-25 transition-transform duration-300 group-hover:scale-105'
-                />
+        {isLoading && <Spinner />}
+        {!isLoading &&
+          shoppingCart?.products?.map((item, index) => (
+            <li key={index} className='justify-between flex gap-2  items-center'>
+              <div className='flex gap-2  items-center'>
+                <div>
+                  <Image
+                    src={'https://heroui.com/images/hero-card-complete.jpeg'}
+                    alt={item.product?.name}
+                    radius='sm'
+                    className='object-cover max-w-25 transition-transform duration-300 group-hover:scale-105'
+                  />
+                </div>
+                <div>
+                  <h3 className='font-semibold'>{item.product?.name}</h3>
+                  <p className='text-gray-500 dark:text-gray-400'>${item.product?.price}</p>
+                  <p className='font-bold dark:text-gray-200'>
+                    ${item.product?.price || 0 * item.quantity}
+                  </p>
+                </div>
               </div>
-              <div>
-                <h3 className='font-semibold'>{item.name}</h3>
-                <p className='text-gray-500 dark:text-gray-400'>${item.price}</p>
-                <p className='font-bold dark:text-gray-200'>${item.totalPrice}</p>
-              </div>
-            </div>
-          </li>
-        ))}
+            </li>
+          ))}
       </CardBody>
     </Card>
   )
