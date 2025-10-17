8b55cfcac03e32b210f7071051a87e70f79bd718
Arviixzuh <arviixzuh@gmail.com>
2025-10-16 21:40:02 -0400

    feat: 🥞 add qualification field to CreateCommentDto with validation

--- backend/src/modules/productComment/dto/create-comment.dto.ts
+++ backend/src/modules/productComment/dto/create-comment.dto.ts
@@ -1,10 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNumber, IsString, MaxLength } from 'class-validator'
+import { IsNumber, IsString, Max, MaxLength, Min } from 'class-validator'
 
 export class CreateCommentDto {
   @ApiProperty({ example: 1, description: 'Id del producto', required: true })
   @IsNumber()
   productId: number
+
+  @ApiProperty({
+    example: 1,
+    description: 'Calificación del producto',
+    required: true,
+  })
+  @IsNumber({}, { message: 'La calificación debe ser un número válido' })
+  @Min(0, { message: 'La calificación mínima permitida es 0' })
+  @Max(5, { message: 'La calificación máxima permitida es 5' })
+  qualification: number
 
   @ApiProperty({
     example: 'Comentario de ejemplo',
