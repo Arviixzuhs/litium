18d38d2fe72fb3d1cebaa5c4fb906a6644b9f3ec
Arviixzuh <arviixzuh@gmail.com>
2025-10-16 21:56:00 -0400

    feat: 🌱 add debounced api fetch for products list

--- frontend/src/modules/home/components/Products/index.tsx
+++ frontend/src/modules/home/components/Products/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { Input } from '@heroui/react'
 import { Search } from 'lucide-react'
-import { products } from './products-mock'
 import { ProductCard } from '../ProductCard'
+import { useDebounce } from 'use-debounce'
+import { ProductModel } from '@/types/productModel'
+import { reqGetProducts } from '@/modules/admin/pages/product/services'
 import { ProductCategories } from '@/modules/product/components/productCategories'
 
 export const Products = () => {
+  const [products, setProducts] = React.useState<ProductModel[]>([])
   const [searchQuery, setSearchQuery] = React.useState('')
+  const [debounceValue] = useDebounce(searchQuery, 100)
+
+  const loadData = async () => {
+    try {
+      const response = await reqGetProducts({
+        name: debounceValue,
+        page: 0,
+        size: 50,
+      })
+      setProducts(response.data.content)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  React.useEffect(() => {
+    loadData()
+  }, [debounceValue])
 
   return (
     <div className='flex gap-4 flex-col'>
