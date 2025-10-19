ac4b2204c4f4eeb43a93e78753572570c2e6915b
Arviixzuh <arviixzuh@gmail.com>
2025-10-18 21:34:24 -0400

    style: 🥞 indentation for improved readability and consistency

--- frontend/src/modules/home/components/Products/hooks/useProductsSearch.ts
+++ frontend/src/modules/home/components/Products/hooks/useProductsSearch.ts
@@ -4,21 +4,21 @@ import { ProductModel } from '@/types/productModel'
 import { reqGetProducts } from '@/modules/admin/pages/product/services'
 
 export function useProductsSearch() {
-    const [products, setProducts] = React.useState<ProductModel[]>([])
-    const [searchQuery, setSearchQuery] = React.useState('')
-    const [debounceValue] = useDebounce(searchQuery, 100)
+  const [products, setProducts] = React.useState<ProductModel[]>([])
+  const [searchQuery, setSearchQuery] = React.useState('')
+  const [debounceValue] = useDebounce(searchQuery, 100)
 
-    React.useEffect(() => {
-        const loadData = async () => {
-            try {
-                const response = await reqGetProducts({ name: debounceValue, page: 0, size: 50 })
-                setProducts(response.data.content)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        loadData()
-    }, [debounceValue])
+  React.useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await reqGetProducts({ name: debounceValue, page: 0, size: 50 })
+        setProducts(response.data.content)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    loadData()
+  }, [debounceValue])
 
-    return { products, searchQuery, setSearchQuery }
+  return { products, searchQuery, setSearchQuery }
 }
