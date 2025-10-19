5a278b69ae0df0562956c96ca1f2862910665bc5
Arviixzuh <arviixzuh@gmail.com>
2025-10-19 19:11:54 -0400

    feat: ✨ add ProductMapper to transform product models with relations into ProductResponseDto

---
+++ backend/src/modules/product/mapper/product.mapper.ts
@@ -0,0 +1,49 @@
+import { BaseMapper } from '@/common/utils/base.mapper'
+import { CategoryMapper } from '@/modules/productCategory/mapper/productCategory.mapper'
+import { SupplierMapper } from '@/modules/productSupplier/mapper/productSupplier.mapper'
+import { Prisma, Product } from '@prisma/client'
+import { ProductResponseDto } from '../dto/product-response.dto'
+
+export type ProductWithRelations = Prisma.ProductGetPayload<{
+  include: {
+    product_x_supplier: {
+      include: {
+        supplier: true
+      }
+    }
+    categories: {
+      include: {
+        category: true
+      }
+    }
+    specifications: true
+  }
+}>
+
+export class ProductMapper extends BaseMapper<Product | ProductWithRelations, ProductResponseDto> {
+  private supplierMapper = new SupplierMapper()
+  private categoryMapper = new CategoryMapper()
+
+  modelToDto(model: Product | ProductWithRelations): ProductResponseDto {
+    return {
+      id: model.id,
+      name: model.name,
+      show: model.show,
+      stock: model.stock,
+      image: model.image,
+      price: model.price,
+      suppliers:
+        'product_x_supplier' in model
+          ? model.product_x_supplier.map((item) => this.supplierMapper.modelToDto(item.supplier))
+          : [],
+      categories:
+        'categories' in model
+          ? model.categories.map((item) => this.categoryMapper.modelToDto(item.category))
+          : [],
+      createdAt: model.createdAt,
+      updatedAt: model.updatedAt,
+      description: model.description,
+      specifications: 'specifications' in model ? model.specifications : [],
+    }
+  }
+}
