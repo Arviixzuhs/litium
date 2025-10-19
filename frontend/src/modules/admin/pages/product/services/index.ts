8dba3573ff08cd7379cc15c046145232e58d7fa2
Arviixzuh <arviixzuh@gmail.com>
2025-10-19 19:22:25 -0400

    refactor: ✨ update create/update product requests to use ProductDto

--- frontend/src/modules/admin/pages/product/services/index.ts
+++ frontend/src/modules/admin/pages/product/services/index.ts
@@ -1,12 +1,12 @@
 import { api } from '@/api/axios'
-import { ProductModel } from '@/types/productModel'
+import { ProductModel, ProductDto } from '@/types/productModel'
 import { PaginatedResponse } from '@/types/paginatedResponse'
 import { ReqGetProductParams } from './interface'
 
 export const reqGetProducts = (params?: ReqGetProductParams) =>
   api.get<PaginatedResponse<ProductModel>>('/product', { params })
-export const reqCreateProduct = (data: Partial<ProductModel>) => api.post('/product', data)
-export const reqUpdateProduct = (id: number, data: Partial<ProductModel>) =>
+export const reqCreateProduct = (data: Partial<ProductDto>) => api.post('/product', data)
+export const reqUpdateProduct = (id: number, data: Partial<ProductDto>) =>
   api.patch(`/product/${id}`, data)
 export const reqDeleteProduct = (id: number) => api.delete(`/product/${id}`)
 export const reqGetProductById = (id: number) => api.get<ProductModel>(`/product/${id}`)
