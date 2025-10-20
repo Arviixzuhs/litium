342e64295e9db026d0123d76fae1032b5ff0d1e4
Arviixzuh <arviixzuh@gmail.com>
2025-10-20 19:23:12 -0400

    feat: 🌱 add InvoiceService to ShoppingCartModule providers

--- backend/src/modules/shoppingCart/shoppingCart.module.ts
+++ backend/src/modules/shoppingCart/shoppingCart.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from '@/prisma/prisma.module'
+import { ProductsModule } from '@/modules/product/product.module'
+import { InvoiceService } from '@/modules/invoice/invoice.service'
 import { ShoppingCartService } from './shoppingCart.service'
 import { ShoppingCartController } from './shoppingCart.controller'
-import { ProductsModule } from '../product/product.module'
 
 @Module({
   imports: [PrismaModule, ProductsModule],
   controllers: [ShoppingCartController],
-  providers: [ShoppingCartService],
+  providers: [ShoppingCartService, InvoiceService],
 })
 export class ShoppingCartModule {}
