791d1e90da8b8bcaf9ca61eb37d6e07608a13782
Arviixzuh <arviixzuh@gmail.com>
2025-10-20 19:22:31 -0400

    feat: 🌱 add shopping cart status update and confirm payment endpoints

--- backend/src/modules/shoppingCart/shoppingCart.controller.ts
+++ backend/src/modules/shoppingCart/shoppingCart.controller.ts
@@ -1,9 +1,10 @@
 import { Request } from 'express'
+import { ShoppingCartStatus } from '@prisma/client'
 import { ShoppingCartService } from './shoppingCart.service'
 import { CreateShoppingCartDto } from './dto/create-shoppingcart.dto'
 import { ShoppingCartFiltersDto } from './dto/shoppingcart-filters.dto'
 import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
-import { Body, Controller, Delete, Get, Param, Post, Query, Req } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Req } from '@nestjs/common'
 
 @ApiTags('Shopping Cart')
 @ApiBearerAuth()
@@ -21,19 +22,35 @@ export class ShoppingCartController {
   @Delete(':shoppingCartId')
   @ApiOperation({ summary: 'Eliminar un carrito de compras' })
   @ApiResponse({ status: 200, description: 'Carrito eliminado correctamente.' })
-  delete(@Param('shoppingCartId') shoppingCartId: number, @Req() req: Request) {
-    return this.shoppingCartService.delete(shoppingCartId, req.user.userId)
+  delete(@Param('shoppingCartId') shoppingCartId: number) {
+    return this.shoppingCartService.delete(shoppingCartId)
   }
 
   @Get(':shoppingCartId')
   @ApiOperation({ summary: 'Obtener un carrito de compras por id' })
-  findById(@Param('shoppingCartId') shoppingCartId: number, @Req() req: Request) {
-    return this.shoppingCartService.findBy(shoppingCartId, req.user.userId)
+  findById(@Param('shoppingCartId') shoppingCartId: number) {
+    return this.shoppingCartService.findBy(shoppingCartId)
   }
 
   @Get()
   @ApiOperation({ summary: 'Obtener carritos de compras del usuario con filtros opcionales' })
   findAll(@Query() query: ShoppingCartFiltersDto) {
     return this.shoppingCartService.findAll(query)
+  }
+
+  @Patch(':shoppingCartId')
+  @ApiOperation({ summary: 'Actualiza el estado de un carrito de compras' })
+  updateShoppingCartStatus(
+    @Param('shoppingCartId') shoppingCartId: number,
+    @Query('status') status: ShoppingCartStatus,
+  ) {
+    return this.shoppingCartService.updateShoppingCartStatus(shoppingCartId, status)
+  }
+
+  @Post(':shoppingCartId/confirm')
+  @ApiOperation({ summary: 'Confirmar pago de un carrito de compras' })
+  @ApiResponse({ status: 200, description: 'Pago confirmado y carrito actualizado.' })
+  async confirmCart(@Param('shoppingCartId') shoppingCartId: number, @Req() req: Request) {
+    return this.shoppingCartService.confirmPay(shoppingCartId, req.user.userId)
   }
 }
