11635e7de6a54f3ae02e33443ae833dc2f793909
Arviixzuh <arviixzuh@gmail.com>
2025-10-20 19:21:09 -0400

    refactor: 📦 update InvoiceMapper to include shopping cart relations and map seller and buyer names

--- backend/src/modules/invoice/mapper/invoice.mapper.ts
+++ backend/src/modules/invoice/mapper/invoice.mapper.ts
@@ -3,33 +3,47 @@ import { BaseMapper } from '@/common/utils/base.mapper'
 import { ProductMapper } from '@/modules/product/mapper/product.mapper'
 import { InvoiceResponseDto } from '../dto/invoice-reponse.dto'
 
-export type InvoicetWithRelations = Prisma.InvoiceGetPayload<{
+export type InvoiceWithRelations = Prisma.InvoiceGetPayload<{
   include: {
-    products: {
+    shoppingCart: {
       include: {
-        product: true
+        products: {
+          include: {
+            product: true
+          }
+        }
+        user: true
       }
     }
     seller: true
   }
 }>
 
-export class InvoiceMapper extends BaseMapper<Invoice | InvoicetWithRelations, InvoiceResponseDto> {
+export class InvoiceMapper extends BaseMapper<Invoice | InvoiceWithRelations, InvoiceResponseDto> {
   private productMapper = new ProductMapper()
-  modelToDto(model: Invoice | InvoicetWithRelations): InvoiceResponseDto {
+
+  modelToDto(model: Invoice | InvoiceWithRelations): InvoiceResponseDto {
+    const firstCart =
+      'shoppingCart' in model && model.shoppingCart.length > 0 ? model.shoppingCart[0] : null
+
+    const sellerName =
+      'seller' in model && model.seller ? `${model.seller.name} ${model.seller.lastName}` : null
+
+    const buyerName = firstCart?.user ? `${firstCart.user.name} ${firstCart.user.lastName}` : null
+
     return {
       id: model.id,
       rif: model.rif ?? null,
       name: model.name,
       phone: model.phone ?? null,
       total: model.total ?? 0,
-      seller: 'seller' in model ? model.seller : null,
-      address: model.address ?? null,
       sellerId: model.sellerId,
-      products:
-        'products' in model
-          ? this.productMapper.modelsToDtos(model.products.map((item) => item.product))
-          : [],
+      sellerName,
+      buyerName,
+      address: model.address ?? null,
+      products: firstCart
+        ? this.productMapper.modelsToDtos(firstCart.products.map((item) => item.product))
+        : [],
       createdAt: model.createdAt,
     }
   }
