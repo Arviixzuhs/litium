44377d89847c7bdd103a024a63292a3b030ead8e
Arviixzuh <arviixzuh@gmail.com>
2025-10-20 19:25:24 -0400

    feat: ✨ add date formatting for createdAt column in RenderCell using getFormattedDateTime

--- frontend/src/components/AppTable/components/RenderCell.tsx
+++ frontend/src/components/AppTable/components/RenderCell.tsx
@@ -1,5 +1,6 @@
 import { Key } from 'react'
 import { DropdownAction } from './DropdownAction'
+import { getFormattedDateTime } from '@/utils/getFormattedDateTime'
 
 export interface RenderCellProps {
   column: Key
@@ -9,6 +10,22 @@ export interface RenderCellProps {
 
 export const RenderCell = (props: RenderCellProps) => {
   switch (props.column) {
+    case 'createdAt':
+      return (
+        <>
+          {getFormattedDateTime({
+            value: props.value,
+            format: {
+              day: '2-digit',
+              month: '2-digit',
+              year: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric',
+            },
+          })}
+        </>
+      )
+
     case 'actions':
       return <DropdownAction itemId={props.itemId} />
 
