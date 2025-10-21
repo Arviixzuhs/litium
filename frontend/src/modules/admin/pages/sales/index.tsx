a45736e70f524d71126c0c81ef707a3a36266b3d
Arviixzuh <arviixzuh@gmail.com>
2025-10-21 00:05:14 -0400

    feat: ✨ add view purchase details modal and dropdownItems in AdminInvoicePage

--- frontend/src/modules/admin/pages/sales/index.tsx
+++ frontend/src/modules/admin/pages/sales/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Info } from 'lucide-react'
 import { AppTable } from '@/components/AppTable'
 import { RootState } from '@/store'
 import { useDebounce } from 'use-debounce'
 import { tableColumns } from './data'
+import { InvoiceModel } from '@/types/invoiceModel'
 import { reqGetInvoices } from './services'
+import { ViewPurchaseModal } from '@/modules/purchases/components/PurchaseCard/components/ViewPurchaseModal'
+import { modalTypes, useModal } from '@/hooks/useModal'
+import { DropdownItemInteface } from '@/components/AppTable/components/DropdownAction'
+import { ShoppingCartProductModel } from '@/types/shoppingCartModel'
 import { useDispatch, useSelector } from 'react-redux'
 import { setTableColumns, setTableData } from '@/features/appTableSlice'
 
@@ -11,6 +17,8 @@ export const AdminInvoicePage = () => {
   const table = useSelector((state: RootState) => state.appTable)
   const dispatch = useDispatch()
   const [debounceValue] = useDebounce(table.filterValue, 100)
+  const [_isOpen, toggleOpen] = useModal(modalTypes.viewPurchases)
+  const [purchaseProducts, setPurchaseProducts] = React.useState<ShoppingCartProductModel[]>([])
 
   const loadData = async () => {
     try {
@@ -35,5 +43,27 @@ export const AdminInvoicePage = () => {
     loadData()
   }, [debounceValue, table.currentPage, table.rowsPerPage, table.dateFilter])
 
-  return <AppTable hiddeAdd filterByDate />
+  const viewDetails = (id: number) => {
+    const currentItem: InvoiceModel = table.data.find((item) => item.id === id)
+    if (!currentItem) return
+
+    setPurchaseProducts(currentItem.products)
+    toggleOpen()
+  }
+
+  const dropdownItems: DropdownItemInteface[] = [
+    {
+      key: 'viewDetails',
+      title: 'Ver detalles',
+      onPress: (itemId) => viewDetails(itemId),
+      startContent: <Info />,
+    },
+  ]
+
+  return (
+    <>
+      <AppTable hiddeAdd filterByDate dropdownItems={dropdownItems} />
+      <ViewPurchaseModal purchaseProducts={purchaseProducts || []} />
+    </>
+  )
 }
