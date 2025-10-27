732c02ad2ab30c6ef36ce06985b9b2d6a88b730e
Arviixzuh <arviixzuh@gmail.com>
2025-10-27 15:53:13 -0400

    feat: 🚚 implement client-side PDF download for sales report

--- frontend/src/modules/admin/pages/sales/services/index.ts
+++ frontend/src/modules/admin/pages/sales/services/index.ts
@@ -1,7 +1,19 @@
 import { api } from '@/api/axios'
 import { InvoiceModel } from '@/types/invoiceModel'
 import { PaginatedResponse } from '@/types/paginatedResponse'
-import { ReqGetSalesParams } from './interface'
+import { ReqDownloadSalesReportParams, ReqGetSalesParams } from './interface'
 
 export const reqGetInvoices = (params?: ReqGetSalesParams) =>
   api.get<PaginatedResponse<InvoiceModel>>('/invoice', { params })
+
+export const reqDownloadSalesReport = async (params?: ReqDownloadSalesReportParams) => {
+  const response = await api.get('/reports/sales', { params, responseType: 'blob' })
+
+  const url = window.URL.createObjectURL(new Blob([response.data]))
+  const link = document.createElement('a')
+  link.href = url
+  link.setAttribute('download', 'reporte_ventas.pdf')
+  document.body.appendChild(link)
+  link.click()
+  link.remove()
+}
