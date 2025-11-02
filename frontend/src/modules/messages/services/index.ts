b461acac68b51b514a313e33847afa7452200806
Arviixzuh <arviixzuh@gmail.com>
2025-11-02 18:09:36 -0400

    refactor: 🧾 type confirm shopping cart response as InvoiceModel

--- frontend/src/modules/messages/services/index.ts
+++ frontend/src/modules/messages/services/index.ts
@@ -1,10 +1,11 @@
 import { api } from '@/api/axios'
+import { InvoiceModel } from '@/types/invoiceModel'
 import { MessageModel } from '@/types/messageModal'
 import { ShoppingCartStatus } from '@/types/shoppingCartModel'
 
 export const reqGetMessagesByCartId = (cartId: number) =>
   api.get<MessageModel[]>(`/messages/cart/${cartId}`)
 export const reqConfirmShoppingCart = (cartId: number) =>
-  api.post(`/shopping-cart/${cartId}/confirm`)
+  api.post<InvoiceModel>(`/shopping-cart/${cartId}/confirm`)
 export const reqUpdateShoppingCartStatus = (cartId: number, status: ShoppingCartStatus) =>
   api.patch(`/shopping-cart/${cartId}?status=${status}`)
