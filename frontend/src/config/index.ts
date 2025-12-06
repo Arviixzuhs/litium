d8caf9b126427e85d4afe3b12960b6ad1ec8bf88
Arviixzuh <arviixzuh@gmail.com>
2025-12-06 12:44:29 -0400

    feat: 💳 add paymentMethods configuration to appConfig

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -1,3 +1,17 @@
 export const appConfig = {
   company: 'Litium C.A',
+  paymentMethods: [
+    {
+      name: "Paypal",
+      metadata: "Paypal data transaction",
+    },
+    {
+      name: "Binance",
+      metadata: "Binance data transaction",
+    },
+    {
+      name: "Transferencia",
+      metadata: "Transfer data transaction",
+    }
+  ]
 }
