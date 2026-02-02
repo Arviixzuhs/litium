b3b99ce9d4f92c3bb7240a00709a465d05266375
Arviixzuh <arviixzuh@gmail.com>
2026-02-01 20:56:15 -0400

    refactor: 🛠️ clean up Navbar component by removing unused imports and restructuring code

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,11 +1,10 @@
 import { RootState } from '@/store'
 import { appConfig } from '@/config'
 import { useSelector } from 'react-redux'
-import { ExternalLink } from 'lucide-react'
 import { NavbarUserOptions } from './components/UserOptions'
+import { ProductSearchInput } from './components/ProductSearchInput'
 import { ShoppingCartSidebar } from '../ShoppingCartSidebar'
 import { ShoppingCartTrigger } from '../ShoppingCartSidebar/components/ShoppingCartTrigger'
-import { CheckPermissionByComponent } from '@/components/CheckPermissionByComponent'
 import {
   Link,
   Button,
@@ -17,6 +16,7 @@ import {
 
 export const Navbar = () => {
   const user = useSelector((state: RootState) => state.user)
+
   return (
     <>
       <HeroUINavbar>
@@ -25,26 +25,17 @@ export const Navbar = () => {
             <p className='font-bold text-inherit'>{appConfig.company}</p>
           </Link>
         </NavbarBrand>
-        <NavbarContent className='hidden sm:flex gap-4' justify='center'></NavbarContent>
+        <NavbarContent className='hidden sm:flex gap-4 w-full' justify='center'>
+          <ProductSearchInput />
+        </NavbarContent>
+        {user && (
+          <NavbarItem>
+            <Link href='/purchases'>Mis compras</Link>
+          </NavbarItem>
+        )}
+
         <NavbarContent justify='end'>
-          {user && (
-            <NavbarItem>
-              <CheckPermissionByComponent permission={'*'} mode='remove'>
-                <Link href='/dashboard' target='_blank' className='flex gap-2'>
-                  Dasboard
-                  <ExternalLink size={15} />
-                </Link>
-              </CheckPermissionByComponent>
-            </NavbarItem>
-          )}
-          {user && (
-            <NavbarItem>
-              <Link href='purchases'>Mis compras</Link>
-            </NavbarItem>
-          )}
-
           <ShoppingCartTrigger />
-
           {!user ? (
             <>
               <NavbarItem className='hidden lg:flex'>
