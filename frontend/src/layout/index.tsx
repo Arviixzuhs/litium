86211e57f2fa0384406f6e825fe01c6fe7052dfc
Arviixzuh <arviixzuh@gmail.com>
2026-02-01 20:59:46 -0400

    feat: 🏗️ implement main Layout with dynamic HeroSection and Back button logic

--- frontend/src/layout/index.tsx
+++ frontend/src/layout/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Back } from '@/components/Back'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/modules/home/components/Footer'
 import { Outlet } from 'react-router-dom'
 import { useLocation } from 'react-router-dom'
+import { HeroSection } from '@/modules/home/components/Hero'
 
 export const Layout = () => {
   const location = useLocation()
@@ -12,11 +14,17 @@ export const Layout = () => {
   }, [location])
 
   return (
-    <div>
+    <div className='flex flex-col min-h-screen'>
       <Navbar />
-      <div className='max-w-5xl w-full mx-auto px-4'>
-        <Outlet />
-      </div>
+      <main className='flex-grow'>
+        {location.pathname === '/' && <HeroSection />}
+        <div className='max-w-5xl w-full mx-auto'>
+          <div className='flex flex-col gap-6 max-w-6xl w-full mx-auto py-12 pb-12 px-4'>
+            {location.pathname !== '/' && <Back />}
+            <Outlet />
+          </div>
+        </div>
+      </main>
       <Footer />
     </div>
   )
