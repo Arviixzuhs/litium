1e6e3dfbbf27abd63eb98eba5f7e4503ff4b442e
Arviixzuh <arviixzuh@gmail.com>
2026-02-01 21:09:18 -0400

    feat: 📖 register CatalogPage and NotFoundPage routes in global router

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,7 +5,10 @@ import { HomePage } from '@/modules/home'
 import { Messages } from '@/modules/messages'
 import { ProductPage } from '@/modules/product'
 import { AdminLayout } from '@/modules/admin/layout'
+import { ScrollToTop } from '@/components/ScrollTop'
+import { CatalogPage } from '@/modules/catalog'
 import { CheckoutPage } from '@/modules/checkout'
+import { NotFoundPage } from '@/modules/404'
 import { Route, Routes } from 'react-router-dom'
 import { DashboardPage } from '@/modules/admin/pages/dashboard'
 import { PurchasesPage } from '@/modules/purchases'
@@ -20,7 +23,6 @@ import { ProtectedRouteAuth } from './middlewares/ProtectedRouteAuth'
 import { ProtectedRouteSession } from './middlewares/ProtectedRouteSession'
 import { CheckPermissionByComponent } from '@/components/CheckPermissionByComponent'
 import { ValidateCheckoutMiddleware } from '@/modules/checkout/middlewares/ValidateCheckoutMiddleware'
-import { ScrollToTop } from '@/components/ScrollTop'
 
 export const Router = () => {
   return (
@@ -30,9 +32,10 @@ export const Router = () => {
           <Route element={<ValidateCheckoutMiddleware />}>
             <Route element={<CheckoutPage />} path='/checkout/:cartId' />
           </Route>
-
           <Route element={<Layout />}>
             <Route element={<HomePage />} path='/' />
+            <Route element={<CatalogPage />} path='/catalog/:catalogId' />
+            <Route element={<NotFoundPage />} path='*' />
             <Route element={<MessagesLayout />}>
               <Route element={<Messages />} path='/messages/:cartId' />
             </Route>
@@ -47,12 +50,12 @@ export const Router = () => {
             }
           >
             <Route element={<DashboardPage />} path='/dashboard' />
-            <Route element={<MessagesAdminPage />} path='/messages/cart/:cartId?' />
-            <Route element={<AdminProductPage />} path='/productos' />
-            <Route element={<AdminSupplierPage />} path='/proveedores' />
-            <Route element={<AdminCategoryPage />} path='/categorias' />
             <Route element={<AdminCatalogPage />} path='/catalogos' />
             <Route element={<AdminInvoicePage />} path='/ventas' />
+            <Route element={<AdminProductPage />} path='/productos' />
+            <Route element={<MessagesAdminPage />} path='/messages/cart/:cartId?' />
+            <Route element={<AdminSupplierPage />} path='/proveedores' />
+            <Route element={<AdminCategoryPage />} path='/categorias' />
           </Route>
         </Route>
         <Route element={<ProtectedRouteAuth />}>
